3b227caac96679c75c4f29fbe3bb3881dcd45108
Mateusz Jabłoński <mail@mateuszjablonski.com>
2025-09-23 14:12:50 +0200

    task 15

---
+++ tasks/15/code.js
@@ -0,0 +1,8 @@
+const users = [
+  { id: 1, username: "Alice", email: "alice@example.com", isActive: true },
+  { id: 2, username: "Bob", email: "bob@example.com", isActive: false },
+  { id: 3, username: "Charlie", email: "alice@example.com", isActive: true },
+  { id: 4, username: "David", email: "david@example.com", isActive: true },
+  { id: 5, username: "Eve", email: "eve@example.com", isActive: false },
+  { id: 6, username: "Frank", email: "frank@example.com", isActive: true },
+];
