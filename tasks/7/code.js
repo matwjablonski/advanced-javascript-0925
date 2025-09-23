734f6f7a26b45907601b6a4130082b8ef8342691
Mateusz Jabłoński <mail@mateuszjablonski.com>
2025-09-23 09:01:34 +0200

    task 8

--- tasks/7/code.js
+++ tasks/7/code.js
@@ -1,6 +1,26 @@
-function getPrice(product) {}
+function getPrice(product) {
+    const prices = {
+        book: 20,
+        laptop: 1000,
+        phone: 500,
+        tablet: 750
+    };
+    return prices[product] || 20000;
+}
 
-function orderProduct() {}
+function orderProduct(
+    productName, 
+    quantity = 1, 
+    pricePerItem = getPrice(productName), 
+    deliveryCost = 10,
+) {
+    return {
+        product: productName,
+        quantity,
+        price: pricePerItem * quantity,
+        deliveryCost: (pricePerItem * quantity) > 100 ? 0 : deliveryCost,
+    }
+}
 
 getPrice('book') // 20
 getPrice('car') // 20000
---
+++ tasks/8/code.js
@@ -0,0 +1,12 @@
+function sumAll() {
+
+}
+
+sumAll(1, 2, 3); // 6
+sumAll(1, "2", 3); // TypeError
+
+function combineArrays() {
+
+}
+
+combineArrays([1, 2], [3, 4], [5]); // [1, 2, 3, 4, 5]
