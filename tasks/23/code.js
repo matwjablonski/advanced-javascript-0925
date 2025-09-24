6a20b30c94fc7e799af4856c60f07a1bfb395617
Mateusz Jabłoński <mail@mateuszjablonski.com>
2025-09-24 12:48:14 +0200

    task 23

---
+++ tasks/23/code.js
@@ -0,0 +1,10 @@
+function calculate(num) {
+
+}
+
+
+console.log(calculate(5)(3)("add"));               // 8
+console.log(calculate(10)(2)(1)("subtract"));      // 7  = 10 - 2 - 1
+console.log(calculate(2)(3)(4)("multiply"));       // 24 = 2*3*4
+console.log(calculate(100)(2)(5)("divide"));       // 10 = 100/2/5
+console.log(calculate(1)(2)(3)(4)(5)("add"));      // 15 = 1+2+3+4+5
